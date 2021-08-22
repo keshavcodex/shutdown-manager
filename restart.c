034d76b9dbf0e24eb760fa7a3b2fd9736da1690d
keshavcodex1 <keshavcodex@gmail.com>
2021-08-23 03:48:01 +0530

    added restart and shutdown

---
+++ restart.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+system( "C:\\WINDOWS\\System32\\shutdown -r -t 0");
+return 0;
+}
---
+++ shutdown.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    //  -t 60 means 60 seconds = 1 minutes, you can change it
+    // Running this code means your computer is going to shutdown in 2 minutes.
+system( "C:\\WINDOWS\\System32\\shutdown -s -t 120");
+return 0;
+}
